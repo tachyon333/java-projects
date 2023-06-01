e4645c89166f91b6662bd8896baef7825795eec7
Ashwani Tiwari <tiwariashwani5534@gmail.com>
2023-06-01 15:59:48 +0530

    In A.java

--- A.java
+++ A.java
@@ -3,7 +3,12 @@ import java.io.*;
 import java.util.*;
 
 class A{
+
+    private static final String TERMINATE  = "Exit";
+    static String name ;
+    static volatile boolean finished = false;
+    
     public static void main(String[] args){
-        
+
     }
 }
